027449202394d450a8a20a24bf5c51112824c7b1
plpa <parulparimadeo@gmail.com>
2022-10-29 21:45:56 +0800

    Integration the scenedelegate and app coordinator

---
+++ CryptoTrack/CryptoTrack/Application/AppCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  AppCoordinator.swift
+//  CryptoTrack
+//
+//  Created by user229653 on 10/29/22.
+//
+
+import Foundation
---
+++ CryptoTrack/CryptoTrack/Application/BaseCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  BaseCoordinator.swift
+//  CryptoTrack
+//
+//  Created by user229653 on 10/29/22.
+//
+
+import Foundation
