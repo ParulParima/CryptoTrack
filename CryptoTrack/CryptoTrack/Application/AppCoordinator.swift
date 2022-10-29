01cc731fc10e25761c19073190767732dce34941
plpa <parulparimadeo@gmail.com>
2022-10-29 21:47:29 +0800

    integrating scene delegate with app coordinator

--- CryptoTrack/CryptoTrack/Application/AppCoordinator.swift
+++ CryptoTrack/CryptoTrack/Application/AppCoordinator.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import UIKit
+
+final class AppCoordinator: BaseCoordinator {
+    
+    var window: UIWindow
+    private(set) var childCoordinator: [BaseCoordinator] = []
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        //initialization of navigation controller and integrating it with window
+        let navigationController = UINavigationController()
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+    }
+    
+    
+}
--- CryptoTrack/CryptoTrack/Application/BaseCoordinator.swift
+++ CryptoTrack/CryptoTrack/Application/BaseCoordinator.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+protocol BaseCoordinator: AnyObject {
+    var childCoordinator: [BaseCoordinator] {get}
+    func start()
+}
